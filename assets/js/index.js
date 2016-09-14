abaeb46753917fec82a22a6d5bedfb8e7d733327
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-14 12:15:53 -0700

    Add structure for modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,14 @@
-alert("hello world");
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById('myBtn');
+
+var span = document.getElementById('mySpan');
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
